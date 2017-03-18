7a7281710f2aca5565e1c4cee6ecc99e00823534
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-18 20:31:57 +0100

    artist show controller and view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params)
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :genre)
+    end
 end
